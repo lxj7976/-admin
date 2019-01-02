b6d26e6a5c9314630a8e0702f83a9c7587db4703
1437715223@qq.com <1437715223@qq.com>
2019-01-02 15:33:51 +0800

    nav样式完成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import "./style/main.less"//导入less  less关联其他的less文件
+import "./style/reset.less"//导入less  less关联其他的less文件
 import App from './App';//导入根组件实例
+import 'antd/dist/antd.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
